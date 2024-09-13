b833cad1a5dad25720974a1580d6db47d2959671
CNfczwhlcole <helong.wang@campus.xaloc.org>
2024-09-13 10:39:21 +0200

    Add files via upload

---
+++ python/ejemplo_variables/ejemplo_variables.py
@@ -0,0 +1,14 @@
+#注释狗都不写
+var_entero=4
+var_dec=3.5
+var_text="hola"
+#傻逼王子鑫
+print (var_entero)
+print (var_dec)
+print (var_text)
+#输出这个玩意的拼接
+print ("El resultado es  ",var_entero,"manzana")
+#拼接
+var_total=var_entero+var_dec
+#输出这个b的拼接
+print (f"El resultado de sumar {var_entero} + {var_dec} es ",var_total)
---
+++ python/ejemplo_variables/ejr.py
@@ -0,0 +1,8 @@
+#注释狗都不写
+var_entero=4
+var_dec=3.5
+var_text="hola"
+
+print (var_entero)
+print (var_dec)
+print (var_text)
